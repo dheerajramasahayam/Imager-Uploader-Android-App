7de060d5ea3c5faa181a745b48bdb37dacc63809
Dheeraj <dheerajreddy.r24@gmail.com>
2022-07-22 14:03:31 +0530

    Added firebase firestore and storage to the application.

--- app/src/main/java/com/hasher/uploader/MainActivity.java
+++ app/src/main/java/com/hasher/uploader/MainActivity.java
@@ -1,14 +1,51 @@
 package com.hasher.uploader;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.hasher.uploader.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ActivityResultLauncher<String> launcher;
+
+    FirebaseFirestore firebaseFirestore;
+    FirebaseStorage firebaseStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        launcher = registerForActivityResult(new ActivityResultContracts.GetContent()
+                , new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        binding.imageView.setImageURI(result);
+
+
+                    }
+                });
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
